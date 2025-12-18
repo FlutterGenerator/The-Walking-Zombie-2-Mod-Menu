c943c8dad886229f22437bd236146e08ab7328dd
tojik_proof_93 <elementman50@yandex.com>
2025-12-18 20:13:05 +0500

    Refactor memory patches for clarity and comments

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -22,9 +22,9 @@
 #include "Includes/Macros.h"
 
 struct My_Patches {
-   
-    MemoryPatch Coins, Silver, Gas, Perks, Skills, AutoHeal, Immortal , Karma, Speed, Damage, Ammo, Spread1, Spread2, Shake1, Shake2, Shake3, Reload, Recoil1, Recoil2, Range, Ads ,AntiDetect, Anticheat1, Anticheat2, Anticheat3, Anticheat4, Anticheat5, Anticheat6, Anticheat7, Anticheat8, Anticheat9, Anticheat10, Antiban1, Antiban2,  Antiban3, Antiban4, Antiban5, Antiban6;
-    
+
+MemoryPatch Coins, Silver, Gas, Perks, Skills, AutoHeal, Immortal , Karma, Speed, Damage, Ammo, Spread1, Spread2, Shake1, Shake2, Shake3, Reload, Recoil1, Recoil2, Range, Ads ,AntiDetect, Anticheat1, Anticheat2, Anticheat3, Anticheat4, Anticheat5, Anticheat6, Anticheat7, Anticheat8, Anticheat9, Anticheat10, Antiban1, Antiban2,  Antiban3, Antiban4, Antiban5, Antiban6;
+
 } hexPatches;
 bool cheat;
 bool Gold = false;
@@ -59,37 +59,37 @@ void *hack_thread(void *) {
 
 #else 
 
-HOOK_LIB("libil2cpp.so", "0x2FE0FE8", GoldX, _Gold);
+HOOK_LIB("libil2cpp.so", "0x2FE0FE8", GoldX, _Gold); // public int get_CoinsGold() { }
 
-//hexPatches.Coins = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0FE8,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1");
-hexPatches.Silver = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0E6C,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1");
-hexPatches.Gas = MemoryPatch::createWithHex("libil2cpp.so",0x2FDFDA8,"1C0644E31EFF2FE1");
-hexPatches.Perks = MemoryPatch::createWithHex("libil2cpp.so",0x30FCC38,"3F0204E30010A0E30F0040E31EFF2FE1");
-hexPatches.Skills = MemoryPatch::createWithHex("libil2cpp.so",0x30FE254,"3F0204E30010A0E30F0040E31EFF2FE1");
-hexPatches.AutoHeal = MemoryPatch::createWithHex("libil2cpp.so",0x2FDDAC8,"1C0644E31EFF2FE1");
-hexPatches.Immortal = MemoryPatch::createWithHex("libil2cpp.so",0x2FF412C,"0000A0E31EFF2FE1");
-hexPatches.Karma = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1E90,"3F0204E30010A0E30F0040E31EFF2FE1");
-hexPatches.Speed = MemoryPatch::createWithHex("libil2cpp.so",0x2FE33E4,"A00044E31EFF2EE1");
-hexPatches.Damage = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5CC,"3F0204E31EFF2FE1");
-hexPatches.Ammo = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD950,"0100A0E31EFF2FE1");
-hexPatches.Spread1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD55C,"0000A0E31EFF2FE1");
-hexPatches.Spread2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD564,"0000A0E31EFF2FE1");
-hexPatches.Shake1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5AC,"0000A0E31EFF2FE1");
-hexPatches.Shake2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5B4,"0000A0E31EFF2FE1");
-hexPatches.Shake3 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5BC,"0000A0E31EFF2FE1");
-hexPatches.Reload = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDB0,"0000A0E31EFF2FE1");
-hexPatches.Recoil1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC0,"0000A0E31EFF2FE1");
-hexPatches.Recoil2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC8,"0000A0E31EFF2FE1");
-hexPatches.Range = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD514,"0000A0E31EFF2FE1");
-hexPatches.Ads = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1678,"0000A0E31EFF2FE1");
+//hexPatches.Coins = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0FE8,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsGold() { }
+hexPatches.Silver = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0E6C,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsSilver() { }
+hexPatches.Gas = MemoryPatch::createWithHex("libil2cpp.so",0x2FDFDA8,"1C0644E31EFF2FE1"); // public float get_Gas() { }
+hexPatches.Perks = MemoryPatch::createWithHex("libil2cpp.so",0x30FCC38,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreePerksPoints() { }
+hexPatches.Skills = MemoryPatch::createWithHex("libil2cpp.so",0x30FE254,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreeSkillsPoints() { }
+hexPatches.AutoHeal = MemoryPatch::createWithHex("libil2cpp.so",0x2FDDAC8,"1C0644E31EFF2FE1"); // public float GetHeal(float pOriginalHeal) { }
+hexPatches.Immortal = MemoryPatch::createWithHex("libil2cpp.so",0x2FF412C,"0000A0E31EFF2FE1"); // public static int GetDamageAgainsPlayer(float pDamageInitial, int pEnemyLevel, int pPlayerLevel) { }
+hexPatches.Karma = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1E90,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_Karma() { }
+hexPatches.Speed = MemoryPatch::createWithHex("libil2cpp.so",0x2FE33E4,"A00044E31EFF2EE1"); // public float get_MovementMultiplier() { }
+hexPatches.Damage = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5CC,"3F0204E31EFF2FE1"); // public int GetDamage() { }
+hexPatches.Ammo = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD950,"0100A0E31EFF2FE1"); // public bool get_IsInfinityAmmoStock() { }
+hexPatches.Spread1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD55C,"0000A0E31EFF2FE1"); // public float get_MinimumSpread() { }
+hexPatches.Spread2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD564,"0000A0E31EFF2FE1"); // public float get_MaximumSpread() { }
+hexPatches.Shake1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5AC,"0000A0E31EFF2FE1"); // public float get_ShakeSpeed() { }
+hexPatches.Shake2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5B4,"0000A0E31EFF2FE1"); // public float get_ShakeDuration() { }
+hexPatches.Shake3 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5BC,"0000A0E31EFF2FE1"); // public float get_ShakeMagnitude() { }
+hexPatches.Reload = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDB0,"0000A0E31EFF2FE1"); // public float get_ReloadTime() { }
+hexPatches.Recoil1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC0,"0000A0E31EFF2FE1"); // public float get_RecoilDuration() { }
+hexPatches.Recoil2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC8,"0000A0E31EFF2FE1"); // public float get_RecoilMagnitude() { }
+hexPatches.Range = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD514,"0000A0E31EFF2FE1"); // public float get_WeaponRange() { }
+hexPatches.Ads = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1678,"0000A0E31EFF2FE1"); // public bool get_AdsEnabled() { }
 
 //AntiDetect
-hexPatches.AntiDetect = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"0000A0E31EFF2FE1");
+hexPatches.AntiDetect = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"0000A0E31EFF2FE1"); // public void add_CheatDetected(Action value) { }
 
 //AntiCheat
-hexPatches.Anticheat1 = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"1E FF 2F E1");
-hexPatches.Anticheat2 = MemoryPatch::createWithHex("libil2cpp.so",0x13933F8,"1E FF 2F E1");
-hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x13934EC,"1E FF 2F E1");
+hexPatches.Anticheat1 = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"1E FF 2F E1"); // public void add_CheatDetected(Action value) { }
+hexPatches.Anticheat2 = MemoryPatch::createWithHex("libil2cpp.so",0x13933F8,"1E FF 2F E1"); // public bool get_IsRunning() { }
+hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x13934EC,"1E FF 2F E1"); // internal virtual void OnCheatingDetected() { }
 //hexPatches.Anticheat4 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD554,"1E FF 2F E1");
 //hexPatches.Anticheat5 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD5F0,"1E FF 2F E1");
 //hexPatches.Anticheat6 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD70C,"1E FF 2F E1");
@@ -99,12 +99,12 @@ hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x13934EC,"1E 
 //hexPatches.Anticheat10 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD988,"1E FF 2F E1");
 
 //AntiBan
-hexPatches.Antiban1 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE24,"0000A0E31EFF2FE1");
-hexPatches.Antiban2 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE50,"0000A0E31EFF2FE1");
-hexPatches.Antiban3 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF128,"0000A0E31EFF2FE1");
-hexPatches.Antiban4 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF3DC,"0000A0E31EFF2FE1");
+hexPatches.Antiban1 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE24,"0000A0E31EFF2FE1"); // private bool get_IsOutOfBoundaries() { }
+hexPatches.Antiban2 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE50,"0000A0E31EFF2FE1"); // public bool GetCheckEnabled() { }
+hexPatches.Antiban3 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF128,"0000A0E31EFF2FE1"); // public void FrozenValueDetected() { }
+hexPatches.Antiban4 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF3DC,"0000A0E31EFF2FE1"); // public void TryCheckCoinBoundaries() { }
 //hexPatches.Antiban5 = MemoryPatch::createWithHex("libil2cpp.so",0xA00390,"0000A0E31EFF2FE1");
-hexPatches.Antiban6 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF560,"1E FF 2F E1");
+hexPatches.Antiban6 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF560,"1E FF 2F E1"); // private void SpeedHackDetected() { }
 
     LOGI(OBFUSCATE("Done"));
 #endif
