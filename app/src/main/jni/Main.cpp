c8ddc9b676f2d6f774bca8d0b2eafb4c7cba9a48
tojik_proof_93 <elementman50@yandex.com>
2025-12-18 21:34:45 +0500

    Update memory patches for game features and anti-cheat

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -59,37 +59,37 @@ void *hack_thread(void *) {
 
 #else 
 
-HOOK_LIB("libil2cpp.so", "0x2FE0FE8", GoldX, _Gold); // public int get_CoinsGold() { }
+HOOK_LIB("libil2cpp.so", "0x30DA1B8", GoldX, _Gold); // public int get_CoinsGold() { } public class CPlayerInfo : CManagerBase
 
-//hexPatches.Coins = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0FE8,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsGold() { }
-hexPatches.Silver = MemoryPatch::createWithHex("libil2cpp.so",0x2FE0E6C,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsSilver() { }
-hexPatches.Gas = MemoryPatch::createWithHex("libil2cpp.so",0x2FDFDA8,"1C0644E31EFF2FE1"); // public float get_Gas() { }
-hexPatches.Perks = MemoryPatch::createWithHex("libil2cpp.so",0x30FCC38,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreePerksPoints() { }
-hexPatches.Skills = MemoryPatch::createWithHex("libil2cpp.so",0x30FE254,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreeSkillsPoints() { }
-hexPatches.AutoHeal = MemoryPatch::createWithHex("libil2cpp.so",0x2FDDAC8,"1C0644E31EFF2FE1"); // public float GetHeal(float pOriginalHeal) { }
-hexPatches.Immortal = MemoryPatch::createWithHex("libil2cpp.so",0x2FF412C,"0000A0E31EFF2FE1"); // public static int GetDamageAgainsPlayer(float pDamageInitial, int pEnemyLevel, int pPlayerLevel) { }
-hexPatches.Karma = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1E90,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_Karma() { }
-hexPatches.Speed = MemoryPatch::createWithHex("libil2cpp.so",0x2FE33E4,"A00044E31EFF2EE1"); // public float get_MovementMultiplier() { }
-hexPatches.Damage = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5CC,"3F0204E31EFF2FE1"); // public int GetDamage() { }
-hexPatches.Ammo = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD950,"0100A0E31EFF2FE1"); // public bool get_IsInfinityAmmoStock() { }
-hexPatches.Spread1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD55C,"0000A0E31EFF2FE1"); // public float get_MinimumSpread() { }
-hexPatches.Spread2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD564,"0000A0E31EFF2FE1"); // public float get_MaximumSpread() { }
-hexPatches.Shake1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5AC,"0000A0E31EFF2FE1"); // public float get_ShakeSpeed() { }
-hexPatches.Shake2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5B4,"0000A0E31EFF2FE1"); // public float get_ShakeDuration() { }
-hexPatches.Shake3 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD5BC,"0000A0E31EFF2FE1"); // public float get_ShakeMagnitude() { }
-hexPatches.Reload = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDB0,"0000A0E31EFF2FE1"); // public float get_ReloadTime() { }
-hexPatches.Recoil1 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC0,"0000A0E31EFF2FE1"); // public float get_RecoilDuration() { }
-hexPatches.Recoil2 = MemoryPatch::createWithHex("libil2cpp.so",0x2FCDDC8,"0000A0E31EFF2FE1"); // public float get_RecoilMagnitude() { }
-hexPatches.Range = MemoryPatch::createWithHex("libil2cpp.so",0x2FCD514,"0000A0E31EFF2FE1"); // public float get_WeaponRange() { }
-hexPatches.Ads = MemoryPatch::createWithHex("libil2cpp.so",0x2FE1678,"0000A0E31EFF2FE1"); // public bool get_AdsEnabled() { }
+//hexPatches.Coins = MemoryPatch::createWithHex("libil2cpp.so",0x30DA1B8,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsGold() { } // public class CPlayerInfo : CManagerBase
+hexPatches.Silver = MemoryPatch::createWithHex("libil2cpp.so",0x30DA03C,"FF 09 0C E3 00 10 A0 E3 9A 0B 43 E3 1E FF 2F E1"); // public int get_CoinsSilver() { } // public class CPlayerInfo : CManagerBase
+hexPatches.Gas = MemoryPatch::createWithHex("libil2cpp.so",0x30D8F78,"1C0644E31EFF2FE1"); // public float get_Gas() { } // public class CPlayerInfo : CManagerBase
+hexPatches.Perks = MemoryPatch::createWithHex("libil2cpp.so",0x31F5EA0,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreePerksPoints() { } // public class CCharacterPerkManager : CManagerBase
+hexPatches.Skills = MemoryPatch::createWithHex("libil2cpp.so",0x31F74BC,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_FreeSkillsPoints() { } // public class CCharacterSkillManager : CManagerBase
+hexPatches.AutoHeal = MemoryPatch::createWithHex("libil2cpp.so",0x30D6C98,"1C0644E31EFF2FE1"); // public float GetHeal(float pOriginalHeal) { } // public class CPlayerCombatValues
+hexPatches.Immortal = MemoryPatch::createWithHex("libil2cpp.so",0x30ED2FC,"0000A0E31EFF2FE1"); // public static int GetDamageAgainsPlayer(float pDamageInitial, int pEnemyLevel, int pPlayerLevel) { } // public static class CGameBalancing
+hexPatches.Karma = MemoryPatch::createWithHex("libil2cpp.so",0x30DB060,"3F0204E30010A0E30F0040E31EFF2FE1"); // public int get_Karma() { } // public class CPlayerInfo : CManagerBase
+hexPatches.Speed = MemoryPatch::createWithHex("libil2cpp.so",0x30DC5B4,"A00044E31EFF2EE1"); // public float get_MovementMultiplier() { } // public class CPlayerInfo : CManagerBase
+hexPatches.Damage = MemoryPatch::createWithHex("libil2cpp.so",0x30C679C,"3F0204E31EFF2FE1"); // public int GetDamage() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Ammo = MemoryPatch::createWithHex("libil2cpp.so",0x30C6B20,"0100A0E31EFF2FE1"); // public bool get_IsInfinityAmmoStock() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Spread1 = MemoryPatch::createWithHex("libil2cpp.so",0x30C672C,"0000A0E31EFF2FE1"); // public float get_MinimumSpread() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Spread2 = MemoryPatch::createWithHex("libil2cpp.so",0x30C6734,"0000A0E31EFF2FE1"); // public float get_MaximumSpread() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Shake1 = MemoryPatch::createWithHex("libil2cpp.so",0x30C677C,"0000A0E31EFF2FE1"); // public float get_ShakeSpeed() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Shake2 = MemoryPatch::createWithHex("libil2cpp.so",0x30C6784,"0000A0E31EFF2FE1"); // public float get_ShakeDuration() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Shake3 = MemoryPatch::createWithHex("libil2cpp.so",0x30C678C,"0000A0E31EFF2FE1"); // public float get_ShakeMagnitude() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Reload = MemoryPatch::createWithHex("libil2cpp.so",0x30C6F80,"0000A0E31EFF2FE1"); // public float get_ReloadTime() { } // [CreateAssetMenu(menuName = "Configs/Items/Projectile")] public class CConfigProjectileWeaponItem : CConfigWeaponItem
+hexPatches.Recoil1 = MemoryPatch::createWithHex("libil2cpp.so",0x30C6F90,"0000A0E31EFF2FE1"); // public float get_RecoilDuration() { } // [CreateAssetMenu(menuName = "Configs/Items/Projectile")] public class CConfigProjectileWeaponItem : CConfigWeaponItem
+hexPatches.Recoil2 = MemoryPatch::createWithHex("libil2cpp.so",0x30C6F98,"0000A0E31EFF2FE1"); // public float get_RecoilMagnitude() { } // [CreateAssetMenu(menuName = "Configs/Items/Projectile")] public class CConfigProjectileWeaponItem : CConfigWeaponItem
+hexPatches.Range = MemoryPatch::createWithHex("libil2cpp.so",0x30C66E4,"0000A0E31EFF2FE1"); // public float get_WeaponRange() { } // public class CConfigWeaponItem : CConfigEquipableItem
+hexPatches.Ads = MemoryPatch::createWithHex("libil2cpp.so",0x30DA848,"0000A0E31EFF2FE1"); // public bool get_AdsEnabled() { } // public class CPlayerInfo : CManagerBase
 
 //AntiDetect
-hexPatches.AntiDetect = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"0000A0E31EFF2FE1"); // public void add_CheatDetected(Action value) { }
+hexPatches.AntiDetect = MemoryPatch::createWithHex("libil2cpp.so",0x1645690,"0000A0E31EFF2FE1"); // public void add_CheatDetected(Action value) { }
 
 //AntiCheat
-hexPatches.Anticheat1 = MemoryPatch::createWithHex("libil2cpp.so",0x1393298,"1E FF 2F E1"); // public void add_CheatDetected(Action value) { }
-hexPatches.Anticheat2 = MemoryPatch::createWithHex("libil2cpp.so",0x13933F8,"1E FF 2F E1"); // public bool get_IsRunning() { }
-hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x13934EC,"1E FF 2F E1"); // internal virtual void OnCheatingDetected() { }
+hexPatches.Anticheat1 = MemoryPatch::createWithHex("libil2cpp.so",0x1645690,"1E FF 2F E1"); // public void add_CheatDetected(Action value) { }
+hexPatches.Anticheat2 = MemoryPatch::createWithHex("libil2cpp.so",0x16457F0,"1E FF 2F E1"); // public bool get_IsRunning() { } // [AddComponentMenu("")] public abstract class ACTkDetectorBase<T> : KeepAliveBehaviour<T>
+hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x16458E4,"1E FF 2F E1"); // internal virtual void OnCheatingDetected() { } // [AddComponentMenu("")] public abstract class ACTkDetectorBase<T> : KeepAliveBehaviour<T>
 //hexPatches.Anticheat4 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD554,"1E FF 2F E1");
 //hexPatches.Anticheat5 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD5F0,"1E FF 2F E1");
 //hexPatches.Anticheat6 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD70C,"1E FF 2F E1");
@@ -99,12 +99,12 @@ hexPatches.Anticheat3 = MemoryPatch::createWithHex("libil2cpp.so",0x13934EC,"1E 
 //hexPatches.Anticheat10 = MemoryPatch::createWithHex("libil2cpp.so",0xFFD988,"1E FF 2F E1");
 
 //AntiBan
-hexPatches.Antiban1 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE24,"0000A0E31EFF2FE1"); // private bool get_IsOutOfBoundaries() { }
-hexPatches.Antiban2 = MemoryPatch::createWithHex("libil2cpp.so",0x32DEE50,"0000A0E31EFF2FE1"); // public bool GetCheckEnabled() { }
-hexPatches.Antiban3 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF128,"0000A0E31EFF2FE1"); // public void FrozenValueDetected() { }
-hexPatches.Antiban4 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF3DC,"0000A0E31EFF2FE1"); // public void TryCheckCoinBoundaries() { }
+hexPatches.Antiban1 = MemoryPatch::createWithHex("libil2cpp.so",0x33D83BC,"0000A0E31EFF2FE1"); // private bool get_IsOutOfBoundaries() { } // public class CPlayerBoundariesManager : CManagerBase
+hexPatches.Antiban2 = MemoryPatch::createWithHex("libil2cpp.so",0x33D83E8,"0000A0E31EFF2FE1"); // public bool GetCheckEnabled() { } // public class CPlayerBoundariesManager : CManagerBase
+hexPatches.Antiban3 = MemoryPatch::createWithHex("libil2cpp.so",0x33D86C0,"0000A0E31EFF2FE1"); // public void FrozenValueDetected() { } // public class CPlayerBoundariesManager : CManagerBase
+hexPatches.Antiban4 = MemoryPatch::createWithHex("libil2cpp.so",0x33D8974,"0000A0E31EFF2FE1"); // public void TryCheckCoinBoundaries() { } // public class CPlayerBoundariesManager : CManagerBase
 //hexPatches.Antiban5 = MemoryPatch::createWithHex("libil2cpp.so",0xA00390,"0000A0E31EFF2FE1");
-hexPatches.Antiban6 = MemoryPatch::createWithHex("libil2cpp.so",0x32DF560,"1E FF 2F E1"); // private void SpeedHackDetected() { }
+hexPatches.Antiban6 = MemoryPatch::createWithHex("libil2cpp.so",0x33D8AF8,"1E FF 2F E1"); // private void SpeedHackDetected() { } // public class CPlayerBoundariesManager : CManagerBase
 
     LOGI(OBFUSCATE("Done"));
 #endif
